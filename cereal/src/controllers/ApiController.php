3e5ec1cc6c372ef4d13c67213fae35a6689b2794
laian <ian.lai@nokia.com>
2017-07-17 21:46:59 -0400

    pls

--- cereal/src/controllers/ApiController.php
+++ cereal/src/controllers/ApiController.php
@@ -11,7 +11,7 @@ class ApiController extends Controller
 
   public function actionGet()
   {
-    $schema = Craft::$app->request->getQuery("schema");
+    $schema = Craft::$app->request->get("schema");
     $this->returnJson($this->schemaToArray($schema));
   }
 
