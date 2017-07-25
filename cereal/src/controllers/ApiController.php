86291c7ca22164f1a6a65bfb0fe3757331a79609
laian <ian.lai@nokia.com>
2017-07-24 21:07:33 -0400

    dev-mode

--- cereal/src/controllers/ApiController.php
+++ cereal/src/controllers/ApiController.php
@@ -15,7 +15,9 @@ class ApiController extends Controller
   public function actionGet()
   {
     $schema = Craft::$app->request->get("schema");
-    $this->returnJson($this->schemaToArray($schema));
+    $this->returnJson($schema)
+
+    //$this->returnJson($this->schemaToArray($schema));
   }
 
   function schemaToArray(array $schema, $relatedEntryModel = null)
