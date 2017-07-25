f82a5ba4c958e7b28742d7426af6ea880bcd4e54
laian <ian.lai@nokia.com>
2017-07-24 21:40:39 -0400

    wip

--- cereal/src/controllers/ApiController.php
+++ cereal/src/controllers/ApiController.php
@@ -71,72 +71,73 @@ class ApiController extends Controller
     $array["id"] = $entry->id;
     foreach ($entry->getFieldLayout()->getFields() as $fieldLayout)
     {
-      $field = $fieldLayout->getField();
-      $handle = $field->getAttributes()["handle"];
-      $type = $field->getFieldType()->getName();
-      switch ($type) {
-        case "Entries":
-          // need to handle multiple
-          $entries = $entry->$handle->find();
-          if (count($entries))
-          {
-            $array[$handle] = array_map(
-              function($entry) {return $this->entryModelToArray($entry);}, 
-              $entries);
-          }
-          else
-          {
-            $array[$handle] = null;
-          }
-          break;
-        case "Assets":
-          $assets = $entry->$handle->find();
-          if (count($assets))
-          {
-            $array[$handle] = array_map(
-              function($asset) {return $asset->getUrl();},
-              $assets);
-          }
-          else
-          {
-            $array[$handle] = null;
-          }
-          break;
-        case "Tags":
-          $tags = $entry->$handle->find();
-          if (count($tags))
-          {
-            $array[$handle] = array_map(
-              function($tag) {return $tag->getTitle();},
-              $tags);
-          }
-          else
-          {
-            $array[$handle] = null;
-          }
-          break;
-        case "Categories":
-          $categories = $entry->$handle->find();
-          if (count($categories))
-          {
-            $array[$handle] = array_map(
-              function($category) {return $category->getTitle();},
-              $categories);
-          }
-          else
-          {
-            $array[$handle] = null;
-          }
-          break;
-        case "Date/Time":
-          $array[$handle] = $entry->$handle;
-          break;
-        case "Plain Text":
-          $array[$handle] = $entry->$handle;
-          break;
-        default: 
-          $array[$handle] = $type;
-      }
+      $array[$fieldLayout->name] = $fieldLayout;
+      // $field = $fieldLayout->getField();
+      // $handle = $field->getAttributes()["handle"];
+      // $type = $field->getFieldType()->getName();
+      // switch ($type) {
+      //   case "Entries":
+      //     // need to handle multiple
+      //     $entries = $entry->$handle->find();
+      //     if (count($entries))
+      //     {
+      //       $array[$handle] = array_map(
+      //         function($entry) {return $this->entryModelToArray($entry);}, 
+      //         $entries);
+      //     }
+      //     else
+      //     {
+      //       $array[$handle] = null;
+      //     }
+      //     break;
+      //   case "Assets":
+      //     $assets = $entry->$handle->find();
+      //     if (count($assets))
+      //     {
+      //       $array[$handle] = array_map(
+      //         function($asset) {return $asset->getUrl();},
+      //         $assets);
+      //     }
+      //     else
+      //     {
+      //       $array[$handle] = null;
+      //     }
+      //     break;
+      //   case "Tags":
+      //     $tags = $entry->$handle->find();
+      //     if (count($tags))
+      //     {
+      //       $array[$handle] = array_map(
+      //         function($tag) {return $tag->getTitle();},
+      //         $tags);
+      //     }
+      //     else
+      //     {
+      //       $array[$handle] = null;
+      //     }
+      //     break;
+      //   case "Categories":
+      //     $categories = $entry->$handle->find();
+      //     if (count($categories))
+      //     {
+      //       $array[$handle] = array_map(
+      //         function($category) {return $category->getTitle();},
+      //         $categories);
+      //     }
+      //     else
+      //     {
+      //       $array[$handle] = null;
+      //     }
+      //     break;
+      //   case "Date/Time":
+      //     $array[$handle] = $entry->$handle;
+      //     break;
+      //   case "Plain Text":
+      //     $array[$handle] = $entry->$handle;
+      //     break;
+      //   default: 
+      //     $array[$handle] = $type;
+      // }
     }
     return $array;
   }
