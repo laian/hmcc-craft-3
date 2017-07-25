145d1b318369aef9f5684284129ba2ae60f77af7
laian <ian.lai@nokia.com>
2017-07-24 21:17:23 -0400

    wip

--- cereal/src/controllers/ApiController.php
+++ cereal/src/controllers/ApiController.php
@@ -31,8 +31,8 @@ class ApiController extends Controller
     }
   */
   {
-    $criteria = new ElementQuery($schema["section"]);
-    unset($schema["section"]);
+    $criteria = new EntryQuery($schema["section"]);
+    // unset($schema["section"]);
     $relatedSchemas = array_key_exists("related", $schema) ? $schema["related"] : null;
     if($relatedSchemas)
       unset($schema->related);
