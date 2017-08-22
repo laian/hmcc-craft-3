f71e33396affad199c62b7798fe4f8a62f3274c6
laian <ian.lai@nokia.com>
2017-08-21 20:41:59 -0400

    testing

--- config/element-api.php
+++ config/element-api.php
@@ -9,10 +9,11 @@ function sermonTransformer(Entry $entry) {
     return [
         'title' => $entry->title,
         'videoUrl' => $entry->videoUrl,
-        'person' => [
-            'name' => $person->title,
-            'photo' => $person->photo
-        ]
+        'person' => gettype($person)    
+        // 'person' => [
+        //     'name' => $person->name,
+        //     'photo' => $person->photo
+        // ]
     ];
 }
 
